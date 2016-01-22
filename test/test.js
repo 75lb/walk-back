58b3d5cb111f192efa69d2e08563ca4b1c4fc48f
Lloyd Brookes <lloyd.brookes@gmail.com>
2016-01-22 12:56:42 +0000

    added support for relative startPaths

--- test/test.js
+++ test/test.js
@@ -21,3 +21,10 @@ test('not found', function (t) {
   var filename = walkBack(__dirname + '/fixture', 'adskjfhladfn')
   t.strictEqual(filename, null)
 })
+
+test('relative path', function (t) {
+  t.plan(1)
+
+  var filename = walkBack('.', 'test/fixture/subdir/file.txt')
+  t.ok(filename && filename.search('walk-back/test/fixture/subdir/file.txt') > 0)
+})
