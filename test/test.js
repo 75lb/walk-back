51b55a36a87fe2e309412f7b44de8830a3dde187
Lloyd Brookes <lloyd.brookes@gmail.com>
2017-03-19 21:14:19 +0000

    remove support for node < v4.0.0

--- test/test.js
+++ test/test.js
@@ -1,44 +1,36 @@
-var test = require('tape')
-var walkBack = require('../')
+const TestRunner = require('test-runner')
+const walkBack = require('../')
+const a = require('assert')
 
-test('basic', function (t) {
-  t.plan(1)
+const runner = new TestRunner()
 
-  var filename = walkBack(__dirname + '/fixture/subdir', 'file.txt')
-  t.ok(filename.search('walk-back/test/fixture/subdir/file.txt') > 0)
+runner.test('basic', function (t) {
+  const filename = walkBack(__dirname + '/fixture/subdir', 'file.txt')
+  a.ok(filename.search('walk-back/test/fixture/subdir/file.txt') > 0)
 })
 
-test('basic2', function (t) {
-  t.plan(1)
-
-  var filename = walkBack(__dirname + '/fixture', 'file.txt')
-  t.ok(filename.search('walk-back/test/fixture/file.txt') > 0)
+runner.test('basic2', function (t) {
+  const filename = walkBack(__dirname + '/fixture', 'file.txt')
+  a.ok(filename.search('walk-back/test/fixture/file.txt') > 0)
 })
 
-test('not found', function (t) {
-  t.plan(1)
-
-  var filename = walkBack(__dirname + '/fixture', 'adskjfhladfn')
-  t.strictEqual(filename, null)
+runner.test('not found', function (t) {
+  const filename = walkBack(__dirname + '/fixture', 'adskjfhladfn')
+  a.strictEqual(filename, null)
 })
 
-test('relative path', function (t) {
-  t.plan(1)
-
-  var filename = walkBack('.', 'test/fixture/subdir/file.txt')
-  t.ok(filename && filename.search('walk-back/test/fixture/subdir/file.txt') > 0)
+runner.test('relative path', function (t) {
+  const filename = walkBack('.', 'test/fixture/subdir/file.txt')
+  a.ok(filename && filename.search('walk-back/test/fixture/subdir/file.txt') > 0)
 })
 
-test('relative path 2', function (t) {
-  t.plan(1)
-
-  var filename = walkBack('./test/fixture/subdir', 'file.txt')
-  t.ok(filename && filename.search('walk-back/test/fixture/subdir/file.txt') > 0)
+runner.test('relative path 2', function (t) {
+  const filename = walkBack('./test/fixture/subdir', 'file.txt')
+  a.ok(filename && filename.search('walk-back/test/fixture/subdir/file.txt') > 0)
 })
 
-test('startPath doesn\'t exist', function (t) {
-  t.throws(function () {
-    walkBack('slfnavnkln', 'file.txt')    
+runner.test('startPath doesn\'t exist', function (t) {
+  a.throws(function () {
+    walkBack('slfnavnkln', 'file.txt')
   })
-  t.end()
 })
