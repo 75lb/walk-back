1880e3af31f62f4b339f0febdf0c64cb94e0bd9a
Lloyd Brookes <lloyd.brookes@gmail.com>
2024-08-23 19:50:23 +0100

    Add CommonJS build

---
+++ test/test.cjs
@@ -0,0 +1,12 @@
+const a = require('assert').strict
+const walkBack = require('walk-back')
+const path = require('node:path')
+
+const test = new Map()
+
+test.set('basic', function (t) {
+  const filename = walkBack('./test/fixture/subdir', 'file.txt')
+  a.ok(filename.indexOf(path.join('walk-back', 'test', 'fixture', 'subdir', 'file.txt')) > 0)
+})
+
+module.exports = { test }
