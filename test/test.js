8db9e63b9a5714c9e4c78691e5edb1a8cdfc89bc
Lloyd Brookes <lloyd.brookes@gmail.com>
2024-08-23 13:13:57 +0100

    fix tests on windows

--- test/test.js
+++ test/test.js
@@ -1,36 +1,35 @@
 import { strict as a } from 'assert'
 import walkBack from 'walk-back'
-import currentModulePaths from 'current-module-paths'
+import path from 'node:path'
 
-const { __dirname } = currentModulePaths(import.meta.url)
 const test = new Map()
 
 test.set('basic', function (t) {
-  const filename = walkBack(__dirname + '/fixture/subdir', 'file.txt')
-  a.ok(filename.search('walk-back/test/fixture/subdir/file.txt') > 0)
+  const filename = walkBack('./test/fixture/subdir', 'file.txt')
+  a.ok(filename.indexOf(path.join('walk-back', 'test', 'fixture', 'subdir', 'file.txt')) > 0)
 })
 
 test.set('basic2', function (t) {
-  const filename = walkBack(__dirname + '/fixture', 'file.txt')
-  a.ok(filename.search('walk-back/test/fixture/file.txt') > 0)
+  const filename = walkBack('./test/fixture', 'file.txt')
+  a.ok(filename.indexOf(path.join('walk-back', 'test', 'fixture', 'file.txt')) > 0)
 })
 
 test.set('not found', function (t) {
-  const filename = walkBack(__dirname + '/fixture', 'adskjfhladfn')
+  const filename = walkBack('./test/fixture', 'adskjfhladfn')
   a.equal(filename, null)
 })
 
 test.set('relative path', function (t) {
-  const filename = walkBack('.', 'test/fixture/subdir/file.txt')
-  a.ok(filename && filename.search('walk-back/test/fixture/subdir/file.txt') > 0)
+  const filename = walkBack('.', path.join('test', 'fixture', 'subdir', 'file.txt'))
+  a.ok(filename && filename.indexOf(path.join('walk-back', 'test', 'fixture', 'subdir', 'file.txt')) > 0)
 })
 
 test.set('relative path 2', function (t) {
-  const filename = walkBack('./test/fixture/subdir', 'file.txt')
-  a.ok(filename && filename.search('walk-back/test/fixture/subdir/file.txt') > 0)
+  const filename = walkBack(path.join('test', 'fixture', 'subdir'), 'file.txt')
+  a.ok(filename && filename.indexOf(path.join('walk-back', 'test', 'fixture', 'subdir', 'file.txt')) > 0)
 })
 
-test.set('startPath doesn\'t exist', function (t) {
+test.set("startPath doesn't exist", function (t) {
   a.throws(function () {
     walkBack('slfnavnkln', 'file.txt')
   })
